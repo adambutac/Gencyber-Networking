5bc39a770914209c4c824f6ac0be6a63b9483ffa
Hendricks <dubmore@gmail.com>
2017-05-08 21:43:25 -1000

    pcaps and files
    removed unneeded files and added required pcaps

---
+++ check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+whereis firefox;
+whereis ifconfig;
+whereis iptables;
+whereis route;
+whereis ssh;
+whereis wget;
+whereis wireshark;
+whereis zenmap;
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install firefox;
+sudo apt install ifconfig;
+sudo apt install iptables;
+sudo apt install route;
+sudo apt install ssh;
+sudo apt install wget;
+sudo apt install wireshark;
+sudo apt install zenmap;
