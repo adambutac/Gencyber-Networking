d7e594f4f78530f543e672b74feb0f7f007998f2
Hendricks <dubmore@gmail.com>
2017-05-09 12:00:07 -1000

    updated shell scripts

--- check.sh
+++ check.sh
@@ -13,11 +13,4 @@ whereis zenmap
 echo
 echo "Program check complete!"
 echo
-echo "Chcking required files..."
-echo
-echo "Directory hash"
-cd ..
-tar -cf - Gencyber-Networking | md5sum
-echo
-echo "File check complete!"
 echo
--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo apt install firefox;
-sudo apt install ifconfig;
-sudo apt install iptables;
-sudo apt install route;
-sudo apt install ssh;
-sudo apt install wget;
-sudo apt install wireshark;
-sudo apt install zenmap;
-
+sudo apt -y update
+sudo apt -fy install
+sudo apt -y install firefox;
+sudo apt -y install git;
+sudo apt -y install ifconfig;
+sudo apt -y install iptables;
+sudo apt -y install route;
+sudo apt -y install ssh;
+sudo apt -y install wget;
+sudo apt -y install wireshark;
+sudo apt -y install zenmap;
